2b20421d70abf2869d95c1595cfb4a47db73f89f
rahulsai121 <pasam.rahul1234@gmail.com>
2024-01-26 19:44:57 +0530

    finalchange

--- store.js
+++ store.js
@@ -1,37 +1,33 @@
 function handleFormSubmit(event) {
-    event.preventDefault();
-  
-    const user = event.target.username.value;
-    const email = event.target.email.value;
-    const phone = event.target.phone.value;
-  
-    //let userDetails = JSON.parse(localStorage.getItem('userdetails')) || [];
-  
-    const newUser = {
-      username: user,
-      email: email,
-      phone: phone,
-    };
-  
-    //userDetails.push(newUser);
-  
+  event.preventDefault();
 
+  const user = event.target.username.value;
+  const email = event.target.email.value;
+  const phone = event.target.phone.value;
 
-    //localStorage.setItem('UserDetails', JSON.stringify(userDetails));
-  
+  const newUser = {
+    username: user,
+    email: email,
+    phone: phone,
+  };
 
-    axios.post('https://crudcrud.com/api/71aad4755a1b4d51b3eea949ad28fbee/data',newUser)
-    .then(res => showOnscreen(res.data))
-    .catch(err =>console.error(err))
+  axios.post('https://crudcrud.com/api/3f7b616e5b794097ae9d64dff26e906b/input',newUser)
+  .then(res => {showOnscreen(res.data);
+    
+    document.querySelector('#username').value='';
+    document.querySelector('#email').value='';
+    document.querySelector('#phone').value='';
 
-    event.target.username.value=' ';
+  })
+  .catch(err =>console.error(err))
+
+event.target.username.value=' ';
     event.target.email.value=' ';
     event.target.phone.value=' ';
-  }
-
+}
 window.addEventListener("DOMContentLoaded",()=>{
-          
-    axios.get("https://crudcrud.com/api/71aad4755a1b4d51b3eea949ad28fbee/data")
+    
+    axios.get("https://crudcrud.com/api/3f7b616e5b794097ae9d64dff26e906b/input")
     .then(res=>{
         console.log('ok')
         for(let i=0;i<res.data.length;i++)
@@ -48,23 +44,57 @@ function showOnscreen(x){
     let i=x._id;
     const ul = document.querySelector('ul');
     const li = document.createElement('li');
-    const liText = document.createTextNode(`${first} ${second} ${third}`);
+    const liText = document.createTextNode(${first} -${second} - ${third});
 
      const button = document.createElement('button');
      button.textContent = 'Delete';
-     button.onclick = function(event) { 
-      axios.delete(`https://crudcrud.com/api/71aad4755a1b4d51b3eea949ad28fbee/data/${i}`)
-       .then(res=>console.log('okok'));
-       event.target.parentNode.remove();
-        };
+     button.id=del${i};
+     button.onclick = function(event) {
+      
+     axios.delete(https://crudcrud.com/api/3f7b616e5b794097ae9d64dff26e906b/input/${i})
+     .then(res=>deleteFunction(i))
+     .catch(err=>console.log(err))
+      };
 
 
 
   const button2 = document.createElement('button');
   button2.textContent = 'Edit';
-  button2.onclick = function(event) {  };
+  button2.id=dele${i}
+  button2.onclick = function(event) {
+   axios.delete(https://crudcrud.com/api/3f7b616e5b794097ae9d64dff26e906b/input/${i})
+  .then(res=>editFunction(first,second,third,i));
+  };
+
+
+
     li.appendChild(liText);
     li.appendChild(button);
     li.appendChild(button2);
     ul.appendChild(li);
     }
+
+
+    function editFunction(na,em,ph,ide){
+
+    const input1=document.querySelector('#username');
+    const input2=document.querySelector('#email');
+    const input3=document.querySelector('#phone');
+
+    input1.value=na;
+    input2.value=em;
+    input3.value=ph;
+    deleteFunction(e${ide})
+  
+    }
+
+
+
+    function deleteFunction(x){
+      let ul=document.querySelector('ul');
+      let child=document.querySelector(#del${x});
+
+      if(child){
+        ul.removeChild(child.parentNode)
+       }
+      }
