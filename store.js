9f6cbc9d605228d2f645835ade30f885aede63fe
rahulsai121 <pasam.rahul1234@gmail.com>
2024-01-26 19:27:25 +0530

    third-3

--- store.js
+++ store.js
@@ -21,7 +21,46 @@ function handleFormSubmit(event) {
   
 
     axios.post('https://crudcrud.com/api/71aad4755a1b4d51b3eea949ad28fbee/data',newUser)
-    .then(res => console.log(res))
+    .then(res => showOnscreen(res.data))
     .catch(err =>console.error(err))
 
+    event.target.username.value=' ';
+    event.target.email.value=' ';
+    event.target.phone.value=' ';
   }
+
+window.addEventListener("DOMContentLoaded",()=>{
+          
+    axios.get("https://crudcrud.com/api/71aad4755a1b4d51b3eea949ad28fbee/data")
+    .then(res=>{
+        console.log('ok')
+        for(let i=0;i<res.data.length;i++)
+        {
+            showOnscreen(res.data[i])
+        }
+    })
+})
+
+function showOnscreen(x){
+    let first=x.username;
+    let second=x.email;
+    let third=x.phone;
+    let i=x._id;
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+    const liText = document.createTextNode(`${first} ${second} ${third}`);
+
+     const button = document.createElement('button');
+     button.textContent = 'Delete';
+     button.onclick = function(event) {    };
+
+
+
+  const button2 = document.createElement('button');
+  button2.textContent = 'Edit';
+  button2.onclick = function(event) {  };
+    li.appendChild(liText);
+    li.appendChild(button);
+    li.appendChild(button2);
+    ul.appendChild(li);
+    }
